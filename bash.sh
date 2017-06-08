e016efc59b9b7246650501e358e285eac504c27f
Max van der Schee <m.v.d.schee@ewake.nl>
2017-06-08 14:44:35 +0200

    changed bash script, uses env var

--- bash.sh
+++ bash.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-APPURL=''
 sed -i "s|    server_name localhost;|    server_name ${APPURL};|" /etc/nginx/sites-available/non-ssl.conf
 sed -i "s|    server_name localhost;|    server_name ${APPURL};|" /etc/nginx/sites-available/ssl.conf

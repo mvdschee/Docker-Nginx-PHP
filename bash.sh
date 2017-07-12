f5af2b6f9c1572b3f15f9987803066111f681fbe
Max van der Schee <m.v.d.schee@ewake.nl>
2017-07-12 23:35:00 +0200

    added extra step in the bash script

--- bash.sh
+++ bash.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#non-ssl
 sed -i "s|    server_name localhost;|    server_name ${APPURL};|" /etc/nginx/sites-available/non-ssl.conf
 sed -i "s|    server_name www.localhost;|    server_name ${SUBAPPURL};|" /etc/nginx/sites-available/non-ssl.conf
 sed -i "s|    return 301 http://localhost$request_uri;|    return 301 http://${APPURL}$request_uri;|" /etc/nginx/sites-available/non-ssl.conf
+
+#ssl
 sed -i "s|    server_name localhost;|    server_name ${APPURL};|" /etc/nginx/sites-available/ssl.conf
 sed -i "s|    server_name www.localhost;|    server_name ${SUBAPPURL};|" /etc/nginx/sites-available/ssl.conf
 sed -i "s|    server_name localhost www.localhost;|    server_name ${APPURL} ${SUBAPPURL};|" /etc/nginx/sites-available/ssl.conf
 sed -i "s|    return 301 https://localhost$request_uri;|    return 301 https://${APPURL}$request_uri;|" /etc/nginx/sites-available/ssl.conf
+
+#extra step just to be sure
+chown container:container /var/www -R

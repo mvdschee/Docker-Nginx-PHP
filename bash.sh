b6c9eca14453e3394fd9a3753c8cc17e17e25302
Max van der Schee <m.v.d.schee@ewake.nl>
2017-09-18 19:41:44 +0200

    changed servername to accept all names

--- bash.sh
+++ bash.sh
@@ -2,21 +2,6 @@
 
 DIR=/etc/letsencrypt/archive
 
-#non-ssl
-echo "Changing non-ssl nginx config"
-
-sed -i "s|    server_name localhost;|    server_name ${APPURL};|" /etc/nginx/sites-available/non-ssl.conf
-sed -i "s|    server_name www.localhost;|    server_name ${SUBAPPURL};|" /etc/nginx/sites-available/non-ssl.conf
-sed -i "s|    return 301 http://localhost$request_uri;|    return 301 http://${APPURL}$request_uri;|" /etc/nginx/sites-available/non-ssl.conf
-
-#ssl
-echo "Changing ssl nginx config"
-
-sed -i "s|    server_name localhost;|    server_name ${APPURL};|" /etc/nginx/sites-available/ssl.conf
-sed -i "s|    server_name www.localhost;|    server_name ${SUBAPPURL};|" /etc/nginx/sites-available/ssl.conf
-sed -i "s|    server_name localhost www.localhost;|    server_name ${APPURL} ${SUBAPPURL};|" /etc/nginx/sites-available/ssl.conf
-sed -i "s|    return 301 https://localhost$request_uri;|    return 301 https://${APPURL}$request_uri;|" /etc/nginx/sites-available/ssl.conf
-
 #extra step just to be sure
 echo "Changing owner of website"
 
@@ -29,15 +14,15 @@ if [ "$CONF" = "ssl.conf" ]; then
   echo "ENV SSL true"
   if [ -d "$DIR/$APPURL" ];then
     echo "Cert already there"
-    sed -i "s|    ssl_certificate /etc/letsencrypt/live/${APPURL}/fullchain.pem;|    ssl_certificate /etc/letsencrypt/live/${APPURL}/fullchain.pem;|" /etc/nginx/sites-available/ssl.conf
-    sed -i "s|    ssl_certificate_key /etc/letsencrypt/live/${APPURL}/privkey.pem;|    ssl_certificate_key /etc/letsencrypt/live/${APPURL}/privkey.pem;|" /etc/nginx/sites-available/ssl.conf
-    sed -i "s|    ssl_trusted_certificate /etc/letsencrypt/live/${APPURL}/chain.pem;|    ssl_trusted_certificate /etc/letsencrypt/live/${APPURL}/chain.pem;|" /etc/nginx/sites-available/ssl.conf
+    sed -i "s|    ssl_certificate /etc/letsencrypt/live/localhost/fullchain.pem;|    ssl_certificate /etc/letsencrypt/live/${APPURL}/fullchain.pem;|" /etc/nginx/sites-available/ssl.conf
+    sed -i "s|    ssl_certificate_key /etc/letsencrypt/live/localhost/privkey.pem;|    ssl_certificate_key /etc/letsencrypt/live/${APPURL}/privkey.pem;|" /etc/nginx/sites-available/ssl.conf
+    sed -i "s|    ssl_trusted_certificate /etc/letsencrypt/live/localhost/chain.pem;|    ssl_trusted_certificate /etc/letsencrypt/live/${APPURL}/chain.pem;|" /etc/nginx/sites-available/ssl.conf
   else
     echo "Adding cert"
     letsencrypt certonly -a webroot --webroot-path=/var/www/app -d  ${APPURL} -d ${SUBAPPURL}
-    sed -i "s|    ssl_certificate /etc/letsencrypt/live/${APPURL}/fullchain.pem;|    ssl_certificate /etc/letsencrypt/live/${APPURL}/fullchain.pem;|" /etc/nginx/sites-available/ssl.conf
-    sed -i "s|    ssl_certificate_key /etc/letsencrypt/live/${APPURL}/privkey.pem;|    ssl_certificate_key /etc/letsencrypt/live/${APPURL}/privkey.pem;|" /etc/nginx/sites-available/ssl.conf
-    sed -i "s|    ssl_trusted_certificate /etc/letsencrypt/live/${APPURL}/chain.pem;|    ssl_trusted_certificate /etc/letsencrypt/live/${APPURL}/chain.pem;|" /etc/nginx/sites-available/ssl.conf
+    sed -i "s|    ssl_certificate /etc/letsencrypt/live/localhost/fullchain.pem;|    ssl_certificate /etc/letsencrypt/live/${APPURL}/fullchain.pem;|" /etc/nginx/sites-available/ssl.conf
+    sed -i "s|    ssl_certificate_key /etc/letsencrypt/live/localhost/privkey.pem;|    ssl_certificate_key /etc/letsencrypt/live/${APPURL}/privkey.pem;|" /etc/nginx/sites-available/ssl.conf
+    sed -i "s|    ssl_trusted_certificate /etc/letsencrypt/live/localhost/chain.pem;|    ssl_trusted_certificate /etc/letsencrypt/live/${APPURL}/chain.pem;|" /etc/nginx/sites-available/ssl.conf
   fi
 else
   echo "Nothing to configure"

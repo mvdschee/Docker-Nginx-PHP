e7c6a063593a23974cfd6f80cd29590c1f5c73c7
Max van der Schee <m.v.d.schee@ewake.nl>
2017-10-29 16:10:23 +0100

    privatekey to key....

--- bash.sh
+++ bash.sh
@@ -9,7 +9,7 @@ if [ "$VIRTUAL_PROTO" = https ]; then
   echo "https for people with sensitive skin"
   mv /etc/nginx/https.conf /etc/nginx/conf.d/default.conf
   sed -i "s|    ssl_certificate /etc/nginx/certs/localhost/fullchain.pem;|    ssl_certificate /etc/nginx/certs/${VIRTUAL_HOST}/fullchain.pem;|" /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_certificate_key /etc/nginx/certs/localhost/privkey.pem;|    ssl_certificate_key /etc/nginx/certs/${VIRTUAL_HOST}/privkey.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_certificate_key /etc/nginx/certs/localhost/key.pem;|    ssl_certificate_key /etc/nginx/certs/${VIRTUAL_HOST}/key.pem;|" /etc/nginx/conf.d/default.conf
   sed -i "s|    ssl_trusted_certificate /etc/nginx/certs/localhost/chain.pem;|    ssl_trusted_certificate /etc/nginx/certs/${VIRTUAL_HOST}/chain.pem;|" /etc/nginx/conf.d/default.conf
 else
   echo "https not enabled"

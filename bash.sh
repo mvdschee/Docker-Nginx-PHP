0cfacec7e1bc4b9bc7dee4860c4c32c50c92e74b
Max van der Schee <m.v.d.schee@ewake.nl>
2017-10-29 15:51:03 +0100

    removing errors

--- bash.sh
+++ bash.sh
@@ -8,9 +8,9 @@ chown container:container /var/www/app -R
 if [ "$VIRTUAL_PROTO" = https ]; then
   echo "https for people with sensitive skin"
   mv /etc/nginx/https.conf /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_certificate /etc/nginx/cert/localhost/fullchain.pem;|    ssl_certificate /etc/nginx/cert/${VIRTUAL_HOST}/fullchain.pem;|" /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_certificate_key /etc/nginx/cert/localhost/privkey.pem;|    ssl_certificate_key /etc/nginx/cert/${VIRTUAL_HOST}/privkey.pem;|" /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_trusted_certificate /etc/nginx/cert/localhost/chain.pem;|    ssl_trusted_certificate /etc/nginx/cert/${VIRTUAL_HOST}/chain.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_certificate /etc/nginx/certs/localhost/fullchain.pem;|    ssl_certificate /etc/nginx/certs/${VIRTUAL_HOST}/fullchain.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_certificate_key /etc/nginx/certs/localhost/privkey.pem;|    ssl_certificate_key /etc/nginx/certs/${VIRTUAL_HOST}/privkey.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_trusted_certificate /etc/nginx/certs/localhost/chain.pem;|    ssl_trusted_certificate /etc/nginx/certs/${VIRTUAL_HOST}/chain.pem;|" /etc/nginx/conf.d/default.conf
 else
   echo "https not enabled"
   mv /etc/nginx/http.conf /etc/nginx/conf.d/default.conf

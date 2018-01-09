b12efcb12cb6516dae51099a28ada89c08344761
Max van der Schee <m.v.d.schee@ewake.nl>
2018-01-09 23:53:30 +0100

    updated bash to accept multiple hostnames

--- bash.sh
+++ bash.sh
@@ -4,13 +4,16 @@
 echo "Changing owner of website"
 chown container:container /var/www/app -R
 
+# Split VIRTUAL_HOST in to multiple items if it is set up this way
+IFS=',' read -r -a array <<< ${VIRTUAL_HOST}
+
 #ssl configuration
 if [ "$VIRTUAL_PROTO" = https ]; then
   echo "https for people with sensitive skin"
   mv /etc/nginx/https.conf /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_certificate /etc/nginx/certs/localhost/fullchain.pem;|    ssl_certificate /etc/nginx/certs/${VIRTUAL_HOST}/fullchain.pem;|" /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_certificate_key /etc/nginx/certs/localhost/key.pem;|    ssl_certificate_key /etc/nginx/certs/${VIRTUAL_HOST}/key.pem;|" /etc/nginx/conf.d/default.conf
-  sed -i "s|    ssl_trusted_certificate /etc/nginx/certs/localhost/chain.pem;|    ssl_trusted_certificate /etc/nginx/certs/${VIRTUAL_HOST}/chain.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_certificate /etc/nginx/certs/localhost/fullchain.pem;|    ssl_certificate /etc/nginx/certs/${array[0]}/fullchain.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_certificate_key /etc/nginx/certs/localhost/key.pem;|    ssl_certificate_key /etc/nginx/certs/${array[0]}/key.pem;|" /etc/nginx/conf.d/default.conf
+  sed -i "s|    ssl_trusted_certificate /etc/nginx/certs/localhost/chain.pem;|    ssl_trusted_certificate /etc/nginx/certs/${array[0]}/chain.pem;|" /etc/nginx/conf.d/default.conf
 else
   echo "https not enabled"
   mv /etc/nginx/http.conf /etc/nginx/conf.d/default.conf
